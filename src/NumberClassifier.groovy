cbcafae239651449f440be2520e085d6655405bd
Teo, Ronald <ronaldteo86@gmail.com>
2013-09-10 15:12:02 -0500

    More functional number classifier

--- src/NumberClassifier.groovy
+++ src/NumberClassifier.groovy
@@ -1,59 +1,36 @@
-
 public class NumberClassifier {
-    private Set<Integer> _factors;
-    private int _number;
 
-    public NumberClassifier(int number) {
-        if (number < 1)
-            throw new IllegalArgumentException(
-            "Can't classify negative numbers");
-        _number = number;
-        _factors = new HashSet<Integer>()
-        _factors.add(1);
-        _factors.add(_number);
+    static public boolean isFactor(int number, int potential_factor) {
+        return number % potential_factor == 0;
     }
 
-    private boolean isFactor(int factor) {
-        return _number % factor == 0;
+    static public Set<Integer> factors(int number) {
+        HashSet<Integer> factors = new HashSet<Integer>();
+        for (int i = 1; i <= sqrt(number); i++)
+            if (isFactor(number, i)) {
+                factors.add(i);
+                factors.add(number / i);
+            }
+        return factors;
     }
 
-    public Set<Integer> getFactors() {
-        return _factors;
-    }
-
-    private void calculateFactors() {
-        for (int i = 1; i <= sqrt(_number) + 1; i++)
-            if (isFactor(i))
-
-                addFactor(i);
-    }
-
-    private void addFactor(int factor) {
-        _factors.add(factor);
-        _factors.add(_number / factor);
-    }
-
-    private int sumOfFactors() {
-        calculateFactors();
+    static public int sum(Set<Integer> factors) {
+        Iterator it = factors.iterator();
         int sum = 0;
-        for (int i : _factors)
-            sum += i;
+        while (it.hasNext())
+            sum += (Integer) it.next();
         return sum;
     }
 
-    public boolean isPerfect() {
-        return sumOfFactors() - _number == _number;
+    static public boolean isPerfect(int number) {
+        return sum(factors(number)) - number == number;
     }
 
-    public boolean isAbundant() {
-        return sumOfFactors() - _number > _number;
+    static public boolean isAbundant(int number) {
+        return sum(factors(number)) - number > number;
     }
 
-    public boolean isDeficient() {
-        return sumOfFactors() - _number < _number;
-    }
-
-    public static boolean isPerfect(int number) {
-        return new NumberClassifier(number).isPerfect();
+    static public boolean isDeficient(int number) {
+        return sum(factors(number)) - number < number;
     }
 }
